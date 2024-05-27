96941b2db6b68c3510447c4eb2dd6b4d211fa01a
AlbertSayos <asayos@fi.uba.ar>
2024-05-27 15:28:09 -0300

    diseño principal agregago

--- app.py
+++ app.py
@@ -14,10 +14,12 @@ app = Flask(__name__)
 mongo_uri = os.getenv("MONGO_CADENACONECCION")
 mongo_db_name = os.getenv("MONGO_DB")
 mongo_collection_name = os.getenv("MONGO_TABLA")
+mongo_collection_regalo = os.getenv("MONGO_REGALOS")
 
 client = MongoClient(mongo_uri)
 db = client[mongo_db_name]
 collection = db[mongo_collection_name]
+listaRegalos = db[mongo_collection_regalo]
 
 @app.route('/')
 def index():
@@ -33,22 +35,41 @@ def perfil(nombre):
     else:
         return "Perfil no encontrado", 404
 
+
+@app.route('/regalos')
+def regalos():
+    lista = listaRegalos.find()
+    print(lista)
+    
+    retorno = []
+    for regalo in lista:
+        if regalo["EsActivo"]:
+            retorno.append(regalo["regalo"])
+    print(retorno)
+    return render_template('index.html')
+
 @app.route('/update_asistencia', methods=['POST'])
 def update_asistencia():
     data = request.get_json()
     nombre = data['nombre']
     
-    # Actualizar la asistencia
+    # Actualizar la asistencia a True
     result = collection.find_one_and_update(
         {"nomFamilia": nombre},
-        {"$set": {"asistencia": not collection.find_one({"nomFamilia": nombre})["asistencia"]}},
+        {"$set": {"asistencia": True}},
         return_document=True
     )
     
     if result:
-        return jsonify({'asistencia': result['asistencia']})
+        # Generar el enlace de WhatsApp con el mensaje predeterminado
+        telefono = os.getenv("NUMTEL")
+        mensaje = "Hola, estoy encantado de asistir a tu baby shower."
+        whatsapp_link = f"https://api.whatsapp.com/send?phone={telefono}&text={mensaje}"
+        
+        return jsonify({'whatsapp_link': whatsapp_link})
     else:
         return jsonify({'error': 'No se pudo actualizar la asistencia'}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=PORT)
